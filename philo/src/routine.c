8fcca4e157f8f5631ebc37656acb2d08858be945
Eve Aviv Keinan <evavkein@gmail.com>
2025-03-01 19:41:45 +0200

    philo_routine prep

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 13:05:38 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/03/01 18:55:43 by ekeinan          ###   ########.fr       */
+/*   Updated: 2025/03/01 19:41:28 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 
 void	*philo_routine(void *arg)
 {
+	t_feast *feast;
 	t_philo *philo;
-	char	c;
 	
 	philo = (t_philo *)arg;
-	c = '0' + philo->id;
-	write(STDOUT_FILENO, &c, 1);
+	feast = philo->feast;
+	while (1)
+	{
+		if (feast->status == COOKING)
+			continue ;
+		if (feast->status == SERVED)
+			break ;
+		if (feast->status == CANCELLED)
+			return (NULL);
+	}
+	while (feast->status != CANCELLED)
+	{
+		
+	}
 	return (NULL);
-	// t_feast *feast;
-
-	// feast = philo->feast;
-	// while (1)
-	// {
-	// 	if (feast->status == COOKING)
-	// 		continue ;
-	// 	if (feast->status == CANCELLED)
-	// 		return ;
-	// 	if (feast->status == SERVED)
-	// 		break ;
-	// }
-	// write(1, philo->id, 1);
 }
